60f02d50f61bbf740cc59318e640f44f5cace89f
vfalconi <vfalconi@bluepeople.com>
2024-04-27 15:55:24 -0500

    Final solution

--- src/main/java/pe/edu/cibertec/WAEC2VictorFalconi2/repository/PersonaRepository.java
+++ src/main/java/pe/edu/cibertec/WAEC2VictorFalconi2/repository/PersonaRepository.java
@@ -1,7 +1,10 @@
 package pe.edu.cibertec.WAEC2VictorFalconi2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.cibertec.WAEC2VictorFalconi2.model.bd.Persona;
 
-public interface PersonaRepository extends JpaRepository {
+@Repository
+public interface PersonaRepository extends JpaRepository<Persona, Integer> {
 
 }
