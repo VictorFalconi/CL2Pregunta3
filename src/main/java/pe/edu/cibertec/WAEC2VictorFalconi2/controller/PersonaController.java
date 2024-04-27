b0feb52d8bc818488e0f5b9fbee66a944817890c
vfalconi <vfalconi@bluepeople.com>
2024-04-27 15:36:47 -0500

    finalizado

---
+++ src/main/java/pe/edu/cibertec/WAEC2VictorFalconi2/controller/PersonaController.java
@@ -0,0 +1,2 @@
+package pe.edu.cibertec.WAEC2VictorFalconi2.controller;public class PersonaController {
+}
--- src/main/java/pe/edu/cibertec/WAEC2VictorFalconi2/model/dto/PersonaRequestDto.java
+++ src/main/java/pe/edu/cibertec/WAEC2VictorFalconi2/model/dto/PersonaRequestDto.java
@@ -2,7 +2,11 @@ package pe.edu.cibertec.WAEC2VictorFalconi2.model.dto;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class PersonaRequestDto {
-
+    private String nompersona;
+    private String apepersona;
+    private Date fechnacpersona;
 }
